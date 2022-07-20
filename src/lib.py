b5efc216cd0e05e35041f313057b768aad77312d
Leonardo Costa <leocosta.loureiro@gmail.com>
2022-07-19 23:54:03 -0300

    corrigida a funcionalidade do algoritmo

--- src/lib.py
+++ src/lib.py
@@ -5,7 +5,6 @@ from matplotlib import pyplot as plt
 from tqdm import tqdm
 from os.path import exists
 
-
 def H(v):
     """Retorna 1 se o parâmetro for maior ou igual a um limiar mínimo
 
@@ -17,7 +16,6 @@ def H(v):
     """
   
     return 1 if v >= 0 else 0
-  
 
 def FindName():
     for i in range(100):
@@ -40,23 +38,36 @@ def ShowData(content, t=0.5, save=True, name=True):
     ax1.set_title('Oscilação de neurônios')
     ax1.set_xlabel('tempo')
     ax1.set_ylabel('Excitação do neurônio')
-    for i in range(len(content)):
-        ax1.plot(content[i])
 
+    for j in range(len(content)):
+        for k in range(len(content[j])):
+            ax1.plot(content[j][k])
+
+    [[[1]]]
+
+    print(len(content))
     discretizado = []
-    for i in range(len(content)):
+    for j in range(len(content)):
         discretizado.append([])
-        for j in range(len(content[i])):
-            discretizado[i].append(1 + 0.1 * i if content[i][j] >= t and content [i][j-1] < t else None)
-
+        for k in range(len(content[j])):
+            discretizado[j].append([])
+            for i in range(len(content[j][k])):
+                if content[j][k][i] >= t and content[j][k][i] > content[j][k][i - 1] and content[j][k][i] > content[j][k][i + 1]:
+                    discretizado[j][k].append(1 + 0.01 * j + 0.001 * k)
+                else:
+                    discretizado[j][k].append(None)
+                    
     ax2 = fig.add_subplot(axgrid[1, 0:])
-    for i in range(len(content)):
-        ax2.scatter(list(range(len(discretizado[i]))), discretizado[i], color='#DD0000')
-
+    for j in range(len(content)):
+        for k in range(len(content[j])):
+            ax2.scatter(list(range(len(discretizado[j][k]))), discretizado[j][k], color='#DD0000')
+    
     if save:
         plt.savefig("data/{}".format(FindName() if name == True else name))
     else:
         plt.show()
+
+
 
 
 def Simulate(DT=0.01, Tmax=1000, width=5, t=0.5, w=0.3, e=0.02, a=6, B=0.1, p=0, I=0.1, o=15, seed=10):
@@ -79,7 +90,6 @@ def Simulate(DT=0.01, Tmax=1000, width=5, t=0.5, w=0.3, e=0.02, a=6, B=0.1, p=0,
     Returns:
         list: dados da simulação
     """
-    print(seed)
     random.seed(seed)
     # Variáveis
     x = []
@@ -99,34 +109,33 @@ def Simulate(DT=0.01, Tmax=1000, width=5, t=0.5, w=0.3, e=0.02, a=6, B=0.1, p=0,
         dx.append([])
         dy.append([])
         S.append([])
+        lx.append([])
+        ly.append([])
         for j in range(width):
             x[i].append(round(random.uniform(-2, 0.4), 2))
             y[i].append(round(random.uniform(0, 4), 2))
             dx[i].append(0)
             dy[i].append(0)
             S[i].append(0)
-    for i in range(width ** 2):
-
-        lx.append([])
-        ly.append([])
-
+            lx[i].append([])
+            ly[i].append([])
 
     iterations = int(Tmax / DT)
 
     for i in tqdm(range(iterations)):
         for j in range(width):
             for k in range(width):
-                S[j][k] = w * (H(x[(j+1) % width][k]) + H(x[(j-1) % width][k]) + H(x[j][(k+1) % width]) + H(x[j][(k-1) % width]))
+                S[j][k] = w * (H(x[(j+1) % width][k] - t) + H(x[(j-1) % width][k] - t) + H(x[j][(k+1) % width] - t) + H(x[j][(k-1) % width] - t))
         for j in range(width):
             for k in range(width):
                 dx[j][k] = (3 * x[j][k] - x[j][k] ** 3 + 2 - y[j][k] + I + p + S[j][k] ) * DT
                 dy[j][k] = (e * ( a * ( 1 + math.tanh(x[j][k] / B)) - y[j][k])) * DT
 
-                if i % o == 0:
-                    lx[(j+1) * (k+1) - 1].append(round(x[j][k], 4))
-
                 x[j][k] += dx[j][k]
                 y[j][k] += dy[j][k]
+
+                if i % o == 0:
+                    lx[j][k].append(round(x[j][k], 4))
         
     return lx
  
--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from lib import *
 import time
+import matplotlib.pyplot as plt
 
 def main():
   #simulação exemplo
 
-  ShowData(Simulate(Tmax=200, w=0.5, o=10, seed=123234))
-  ShowData(Simulate(Tmax=200, w=0.5, o=10, seed=int(32452345)))
-  ShowData(Simulate(Tmax=200, w=0.5, o=10, seed=int(928734)))
+  ShowData(Simulate(Tmax=2000, w=0.2, o=100, width=20, seed=123234, I=0.5), save=True)
+
 
 if __name__ == "__main__":
   main()
